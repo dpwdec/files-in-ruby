c03b5af6a3964c95b61696023aaefea5018d21f5
Declan <declankolakowski@gmail.com>
2020-03-31 10:30:43 +0000

    created directory and file tests

---
+++ scripts/filing.rb
@@ -0,0 +1,25 @@
+file = File.open("../resources/gems.txt")
+
+file_data = file.readlines.map(&:chomp)
+
+print file_data
+
+file_data = File.open("../info.txt").read
+
+print file_data
+
+File.foreach("../resources/gems.txt") do |line|
+    puts "gem #{line}"
+end
+
+#File.open("book.txt", "a") { |f| f.write "#{Time.now}" }
+
+#File.write("book.txt", "#{Time.now}", mode: "w")
+
+#the_time = Time.now.to_s
+#File.write("book.txt", the_time)
+
+entries = Dir.glob("**/**")
+
+print entries
+
